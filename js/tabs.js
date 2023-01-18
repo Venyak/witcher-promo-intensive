454f1f55af1c247441a71efccf1b5ccb0282bfe0
Лида Рудова <102348531+Venyak@users.noreply.github.com>
2023-01-18 18:04:58 +0500

    Update tabs.js

--- js/tabs.js
+++ js/tabs.js
@@ -1,8 +1,16 @@
 const tabs = (param) => {
-  const { selectorTabsBtn, activeClassBtn, selectorTabsItem, activeClassTab } = param;
+  const {
+    selectorTabsBtn,
+    activeClassBtn,
+    selectorTabsItem,
+    activeClassTab,
+    callback = () => {},
+  } = param;
 
   const tabsBtns = document.querySelectorAll(selectorTabsBtn);
   const tabsItems = document.querySelectorAll(selectorTabsItem);
+
+  tabsBtns.forEach(() => {});
 };
 
 export default tabs;
