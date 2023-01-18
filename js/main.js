64a8d6ec50bc50224646a6d942a9750e66e8d0e2
Лида Рудова <102348531+Venyak@users.noreply.github.com>
2023-01-18 17:33:53 +0500

    tabs.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 import burgerMenu from './burger.js';
+import tabs from './tabs.js';
 
 burgerMenu({
   selectorMenu: '.nav__list',
   selectorOpenMenu: 'nav__list-active',
+});
+
+tabs({
+  selectorTabsBtn: '.tabs__btn',
+  activeClassBtn: 'tabs__btn-active',
+  selectorTabsItem: '.tabs__item',
+  activeClassTab: '.tabs__item-active',
 });
---
+++ js/tabs.js
@@ -0,0 +1,5 @@
+const tabs = (param) => {
+  console.log('tabs');
+};
+
+export default tabs;
