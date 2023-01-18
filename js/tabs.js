f81543c2c4813f132faf7ee7ddf922562b3c3a48
Лида Рудова <102348531+Venyak@users.noreply.github.com>
2023-01-18 18:33:23 +0500

    update tabs.js

--- js/tabs.js
+++ js/tabs.js
@@ -10,8 +10,18 @@ const tabs = (param) => {
   const tabsBtns = document.querySelectorAll(selectorTabsBtn);
   const tabsItems = document.querySelectorAll(selectorTabsItem);
 
-  tabsBtns.forEach((btn, i) => {
-    console.log(btn, i);
+  tabsBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      for (let i = 0; i < tabsBtns.length; i++) {
+        if (tabsBtns[i] === btn) {
+          tabsBtns[i].classList.add(activeClassBtn);
+          tabsItems[i].classList.add(activeClassTab);
+        } else {
+          tabsBtns[i].classList.remove(activeClassBtn);
+          tabsItems[i].classList.remove(activeClassTab);
+        }
+      }
+    });
   });
 };
 
