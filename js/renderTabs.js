0846eb4a0c4f21887261193458bc27d1683c8ddb
Лида Рудова <102348531+Venyak@users.noreply.github.com>
2023-01-20 15:24:39 +0500

    Update renderTabs.js

--- js/renderTabs.js
+++ js/renderTabs.js
@@ -1,11 +1,21 @@
 import getData from './getData.js';
 
+const getCard = function (dataCard) {
+  console.log(dataCard);
+};
+
 const renderTabs = async (i = 0) => {
   const tabsContent = document.querySelectorAll('.tabs__content');
 
   const type = tabsContent[i].dataset.base;
 
-  const data = getData;
+  const data = await getData(`db/${type}.json`);
+
+  const listElem = data.map(getCard, type);
+
+  tabsContent[i].textContent = '';
+
+  tabsContent[i].append(...listElem);
 };
 
 export default renderTabs;
